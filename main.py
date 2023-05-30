2b98263c9229ac54597481f5396ccf8ba4301489
AgataSedlaczek <navi2@o2.pl>
2023-05-30 13:00:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+print("Cześć, jestem Twoim pierwszym programem")
+
+
+def mojtekst():
+  print("Cześć, to ja Twoja funkcja")
+
+
+mojtekst()
+
+
+def pozdrowienia():
+  print("Pozdrowienia z mojej funkcji!")
+
+pozdrowienia()
+
+def dodawanie():
+  return 2 + 5
+
+print("Wynik mojej funkcji dodawania to:", dodawanie())
+
+
+dodawanie()
+
+
+x="Agata"
+y=1986
+
+
+print("Cześć", x, "rok twojego urodzenia to", y)
+
+
+
+
+
